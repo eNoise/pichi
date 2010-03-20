fcd9619f93f1e150e67c43033d1a78d56c791bc2
DEgx <deg@uruchie.org>
2010-03-20 10:58:57 +0000

    fixed statuses
    git-svn-id: http://pichi.googlecode.com/svn/trunk@91 f69a27dc-0b97-11df-8612-a59a22e72e9b

--- main.php
+++ main.php
@@ -186,6 +186,8 @@ while(!$jabber->isDisconnected()) {
 				break;
 			case 'presence':
 				$log->log("Recive PRESENCE Handler from: {$data['from']} [{$data['show']}] {$data['status']}",PichiLog::LEVEL_DEBUG);
+				if($data['show'] != 'unavailable')
+					$data['show'] = 'available'; // Во всех случаях кроме оффлайна, ставим онлайн
 				if($data['show'] == 'available' || $data['show'] == 'unavailable')
 				{
 					unset($jid);
