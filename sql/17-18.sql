e5b21400abdd327df708920fdb440469e334b617
DEgx <deg@uruchie.org>
2010-09-11 10:56:28 +0000

    new struct
    git-svn-id: http://pichi.googlecode.com/svn/trunk@243 f69a27dc-0b97-11df-8612-a59a22e72e9b

--- sql/17-18.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE users_nick (`jid` TEXT, `nick` TEXT, `room` TEXT, `time` TEXT);
-UPDATE db_version SET version=18;
--- sql/readme.txt
+++
@@ -1,2 +0,0 @@
-This directory contains only patch's. 
-DO NOT IMPORT ANY SQL FILES WHEN YOU INSTALL PICHI.
