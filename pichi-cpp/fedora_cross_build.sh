b2fe35e5b22d07cf86c5d838040de21654c79fce
DEgx <deg@uruchie.org>
2010-10-18 13:13:54 +0000

    Windows Cross-compile
    git-svn-id: http://pichi.googlecode.com/svn/trunk@268 f69a27dc-0b97-11df-8612-a59a22e72e9b

---
+++ pichi-cpp/fedora_cross_build.sh
@@ -0,0 +1,13 @@
+rev_ver=`svn info|grep Revision|cut -d: -f2|awk '{print $1}'`
+
+rm -fr win32build
+mkdir win32build
+cd win32build
+mkdir pichi
+mingw32-cmake -DCMAKE_BUILD_TYPE=Release -DLOCAL_GLOOX=ON -DLOCAL_TINYXML=ON -DCMAKE_INSTALL_PREFIX=`pwd`/pichi ../
+mingw32-make -j4
+mingw32-make install
+cd pichi
+cp /usr/i686-pc-mingw32/sys-root/mingw/bin/{boost_regex-gcc44-mt-1_41.dll,libsqlite3-0.dll,libgcc_s_sjlj-1.dll} ./
+cd ../
+zip -r pichi_r${rev_ver}.zip EiskaltDC++/
