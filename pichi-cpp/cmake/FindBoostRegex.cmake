cf331d4d765fccc9b6ae86aaef98f4f31dfcdff3
DEgx <deg@uruchie.org>
2010-10-18 12:01:10 +0000

    windows fixes
    git-svn-id: http://pichi.googlecode.com/svn/trunk@266 f69a27dc-0b97-11df-8612-a59a22e72e9b

--- pichi-cpp/cmake/FindBoostRegex.cmake
+++ pichi-cpp/cmake/FindBoostRegex.cmake
@@ -1,6 +1,12 @@
 # - Try to find Boost-regex lib
 
-FIND_LIBRARY( BOOST_REGEX_LIBRARIES ${Boost_LIB_PREFIX}boost_regex${_boost_MULTITHREADED}-${Boost_LIB_VERSION} )
+if(WIN32)
+	if(Boost_FOUND)
+		set(BOOST_REGEX_LIBRARIES ${Boost_LIB_PREFIX}boost_regex-gcc44${_boost_MULTITHREADED}-${Boost_LIB_VERSION} )
+	endif(Boost_FOUND)
+else(WIN32)
+	FIND_LIBRARY( BOOST_REGEX_LIBRARIES ${Boost_LIB_PREFIX}boost_regex${_boost_COMPILER}${_boost_MULTITHREADED}-${Boost_LIB_VERSION} )
+endif(WIN32)
 
 if( BOOST_REGEX_LIBRARIES )
 	message( STATUS "Found boost-regex: ${BOOST_REGEX_LIBRARIES}" )
