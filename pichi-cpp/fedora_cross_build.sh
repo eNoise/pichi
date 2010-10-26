914147787d21fb46f25e1ea9007fc74f785760f1
DEgx <deg@uruchie.org>
2010-10-26 16:44:38 +0000

    win32 fix
    git-svn-id: http://pichi.googlecode.com/svn/trunk@278 f69a27dc-0b97-11df-8612-a59a22e72e9b

--- pichi-cpp/fedora_cross_build.sh
+++ pichi-cpp/fedora_cross_build.sh
@@ -9,6 +9,6 @@ mingw32-make -j4
 mingw32-make install
 cd pichi
 i686-pc-mingw32-strip pichi.exe
-cp /usr/i686-pc-mingw32/sys-root/mingw/bin/{boost_regex-gcc44-mt-1_41.dll,libsqlite3-0.dll,libgcc_s_sjlj-1.dll} ./
+cp /usr/i686-pc-mingw32/sys-root/mingw/bin/{boost_regex-gcc44-mt-1_41.dll,libsqlite3-0.dll,libgcc_s_sjlj-1.dll,libcurl-4.dll} ./
 cd ../
 zip -r pichi_r${rev_ver}.zip pichi/
