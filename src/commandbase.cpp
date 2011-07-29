3f6f552f6f89d417a1f25eac6c31e94c8be5c65e
DEgx <deg@uruchie.org>
2011-07-29 18:11:55 +0300

    начинаем работать опять

--- src/commandbase.cpp
+++ src/commandbase.cpp
@@ -1,6 +1,6 @@
 /*
     Pichi XMPP (Jabber) Bot.
-    Copyright (C) 2010  Alexey Kasyanchuk (deg@uruchie.org)
+    Copyright (C) 2011  Alexey Kasyanchuk (deg@uruchie.org)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
