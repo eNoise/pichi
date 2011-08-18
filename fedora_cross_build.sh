7602717d6aa5c4d4415a6ae812cf58eb2fb1f53a
DEgx <deg@uruchie.org>
2011-08-18 03:39:12 +0300

    oops

--- fedora_cross_build.sh
+++ fedora_cross_build.sh
@@ -12,7 +12,7 @@ mingw32-make -j4
 mingw32-make install
 cd pichi
 i686-pc-mingw32-strip pichi.exe
-cp /usr/i686-pc-mingw32/sys-root/mingw/bin/{boost_regex-${PICHI_BOOST_GCC}-mt-${PICHI_BOOST_VERSION}.dll,boost_program_options-${PICHI_BOOST_GCC}-mt-${PICHI_BOOST_VERSION}.dll,libsqlite3-0.dll,libgcc_s_sjlj-1.dll,libcurl-4.dll,libstdc++-6.dll,libcrypto-10.dll,libidn-11.dll,libssh2-1.dll,zlib1.dll,libiconv-2.dll,libintl-8.dll,libssl-10.dll,pthreadGC2.dll} ./
+cp /usr/i686-pc-mingw32/sys-root/mingw/bin/{boost_regex-${PICHI_BOOST_GCC}-mt-${PICHI_BOOST_VERSION}.dll,boost_program_options-${PICHI_BOOST_GCC}-mt-${PICHI_BOOST_VERSION}.dll,libsqlite3-0.dll,libgcc_s_sjlj-1.dll,libcurl-4.dll,libstdc++-6.dll,libcrypto-10.dll,libidn-11.dll,libssh2-1.dll,zlib1.dll,libiconv-2.dll,libintl-8.dll,libssl-10.dll,pthreadGC2.dll,mingwm10.dll} ./
 i686-pc-mingw32-strip libstdc++-6.dll
 cd ../
 zip -r pichi-r`git log --pretty=format:'' | wc -l`.zip pichi/
