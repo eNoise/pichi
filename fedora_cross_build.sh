5f7def3a1b90c709c044eeedb6e8591e5f96bacc
DEgx <deg@uruchie.org>
2011-08-20 06:27:49 +0300

    new mingw

--- fedora_cross_build.sh
+++ fedora_cross_build.sh
@@ -9,13 +9,13 @@ cd win32build
 mkdir pichi
 mingw32-cmake -DCMAKE_BUILD_TYPE=Release -DLOCAL_GLOOX=ON -DLOCAL_TINYXML=ON -DLOCAL_LUA=ON -DWITH_LUA=ON -DCMAKE_INSTALL_PREFIX=`pwd`/pichi ../
 mingw32-make -j4
-cp /usr/i686-pc-mingw32/sys-root/mingw/bin/{boost_regex-${PICHI_BOOST_GCC}-mt-${PICHI_BOOST_VERSION}.dll,boost_program_options-${PICHI_BOOST_GCC}-mt-${PICHI_BOOST_VERSION}.dll,libsqlite3-0.dll,libgcc_s_sjlj-1.dll,libcurl-4.dll,libstdc++-6.dll,libcrypto-10.dll,libidn-11.dll,libssh2-1.dll,zlib1.dll,libiconv-2.dll,libintl-8.dll,libssl-10.dll,pthreadGC2.dll,mingwm10.dll} ./
+cp /usr/i686-pc-mingw32/sys-root/mingw/bin/{boost_regex-${PICHI_BOOST_GCC}-mt-${PICHI_BOOST_VERSION}.dll,boost_program_options-${PICHI_BOOST_GCC}-mt-${PICHI_BOOST_VERSION}.dll,libsqlite3-0.dll,libgcc_s_sjlj-1.dll,libcurl-4.dll,libstdc++-6.dll,libcrypto-10.dll,libidn-11.dll,libssh2-1.dll,zlib1.dll,iconv.dll,libintl-8.dll,libssl-10.dll,pthreadGC2.dll,mingwm10.dll} ./
 mingw32-make test
 mingw32-make install
 cp -R Testing/Temporary pichi/testinglog
 cd pichi
 i686-pc-mingw32-strip pichi.exe
-cp /usr/i686-pc-mingw32/sys-root/mingw/bin/{boost_regex-${PICHI_BOOST_GCC}-mt-${PICHI_BOOST_VERSION}.dll,boost_program_options-${PICHI_BOOST_GCC}-mt-${PICHI_BOOST_VERSION}.dll,libsqlite3-0.dll,libgcc_s_sjlj-1.dll,libcurl-4.dll,libstdc++-6.dll,libcrypto-10.dll,libidn-11.dll,libssh2-1.dll,zlib1.dll,libiconv-2.dll,libintl-8.dll,libssl-10.dll,pthreadGC2.dll,mingwm10.dll} ./
+cp /usr/i686-pc-mingw32/sys-root/mingw/bin/{boost_regex-${PICHI_BOOST_GCC}-mt-${PICHI_BOOST_VERSION}.dll,boost_program_options-${PICHI_BOOST_GCC}-mt-${PICHI_BOOST_VERSION}.dll,libsqlite3-0.dll,libgcc_s_sjlj-1.dll,libcurl-4.dll,libstdc++-6.dll,libcrypto-10.dll,libidn-11.dll,libssh2-1.dll,zlib1.dll,iconv.dll,libintl-8.dll,libssl-10.dll,pthreadGC2.dll,mingwm10.dll} ./
 i686-pc-mingw32-strip libstdc++-6.dll
 cd ../
 zip -r pichi-r`git log --pretty=format:'' | wc -l`.zip pichi/
