fd455a684a430ca9151c6f989e88bfa796f52f57
DEgx <deg@uruchie.org>
2011-09-13 00:06:45 +0300

    forum karma

--- lua/uruchie_forum.lua
+++ lua/uruchie_forum.lua
@@ -37,6 +37,8 @@ function PichiCommands.forum ( arg, pichiobject )
 !forum - это подсказка
 !forum whoami - к какому аккаунту я привязан
 !forum login user password - связать себя с аккаунтом форума
+!forum getpost [тема] - получить посты
+!forum setkarma user +1|-1 - поднять, опустить карму
 ]] )
 	end
 end
@@ -62,7 +64,7 @@ end
 function UruchieForumCommands.login ( args, pichiobject )
 	local user, password
 	if type(args) == "string" then
-		user, password = string.match(args, "(%a+) (%a+)")
+		user, password = string.match(args, "([%a0-9А-Яа-я]+) ([%a0-9А-Яа-я]+)")
 	end
 	if not user or not password then
 		SendAnswer( pichiobject, "введите полную информацию и попробуйте опять")
@@ -71,6 +73,10 @@ function UruchieForumCommands.login ( args, pichiobject )
 	SetJIDinfo( pichiobject, GetLastJID( pichiobject ), "uforum_user", user )
 	SetJIDinfo( pichiobject, GetLastJID( pichiobject ), "uforum_password", password )
 	SendAnswer( pichiobject, "установлено")
+end
+
+local function escapePagetext( text )
+  return string.gsub( text, "\[/?[A-Za-z]+\]" , "" )
 end
 
 function UruchieForumCommandsAsync.getpost( args, pichiobject )
@@ -86,14 +92,57 @@ function UruchieForumCommandsAsync.getpost( args, pichiobject )
 	 local answer = "\n"
 	 for k,post in pairs(getposts.posts) do
 		answer = answer 
-			  .. "-------------\n"
-			  .. (thread or "Тема: " .. post.thread.title .. "\n")
-			  .. post.pagetext 
-			  .. "\n"
+			  .. "Тема: " .. post.thread.title .. "\n"
+			  .. "Автор поста: " .. post.username .. "\n"
+			  .. "Время: " .. os.date("%d.%m.%Y %H:%M", post.dateline) .. "\n"
+			  .. "Заголовок: " .. (post.title or "<нет>") .. "\n"
+			  .. escapePagetext(post.pagetext) 
+			  .. "\n\n"
 	 end
 	 SendAnswer(pichiobject, answer)
 end
 
-function UruchieForumCommands.test( args, pichiobject )
+function UruchieForumCommandsAsync.setkarma( args, pichiobject )
+	 local target, sub
+	 if type(args) == "string" then
+		target, sub = string.match(args, "(%a+) ([1+-]+)")
+	 end
+	 if sub ~= "+1" and sub ~= "-1" then
+		SendAnswer(pichiobject, "проверьте введенные параметры")
+		return
+	 end
+	 local user = GetJIDinfo( pichiobject, GetLastJID( pichiobject ), "uforum_user" )
+	 local password = GetJIDinfo( pichiobject, GetLastJID( pichiobject ), "uforum_password" )
+	 user = user.uforum_user
+	 password = password.uforum_password
+	 if not user or not password then
+		SendAnswer(pichiobject, "установите свои данные")
+		return
+	 end
+	 local gertarget = ReadUrl("http://uruchie.org/api.php", {
+			module = "forum",
+			action = "username2userid",
+			username = target
+		})
+	 target = JsonDecode(gertarget)
+	 target = target.userid
+	 if not target or target == "false" or target == "0" then
+		SendAnswer(pichiobject, "неверный ник")
+	 end
+	 local karma = ReadUrl("http://uruchie.org/api.php", {
+			module = "forum",
+			action = (sub == "+1" and "pluskarma") or "minuskarma",
+			username = user,
+			password = md5sum(password),
+			targetuserid = target
+		})
+	karma = JsonDecode(Utf8Decode(karma))
+	if karma.error.error == "true" then
+		SendAnswer(pichiobject, "произошла ошибка: " .. karma.error.description)
+	else
+		SendAnswer(pichiobject, "Карма успешно изменена")
+	end
+end
 
+function UruchieForumCommands.test( args, pichiobject )
 end
